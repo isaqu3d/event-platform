29506c0d902ad6582bc02e52c1c3ae2502f8418f
Isaque.S <isaqueblack129@gmail.com>
2022-06-24 16:41:08 -0300

    video,router,video-description,teacher

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { ApolloProvider } from '@apollo/client'
+import { Router } from './components/router/Router'
+import { client } from './lib/apollo'
 import { Event } from './pages/Event'
 
 function App() {
   return (
     <div>
-      <Event />
+      <ApolloProvider client={client}>
+        <Router />
+      </ApolloProvider>
     </div>
   )
 }
