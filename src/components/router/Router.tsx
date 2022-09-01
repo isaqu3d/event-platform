07db65c283c24e95219d80c4cc1b8a1f0864c525
Isaque <isaque.error404@gmail.com>
2022-09-01 12:13:29 -0300

    add footer

--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -1,13 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
-import { Event } from '../../pages/Event'
-import { Subscribe } from '../../pages/Subscribe'
+import { Route, Routes } from "react-router-dom";
+import { Event } from "../../pages/Event";
+import { Subscribe } from "../../pages/Subscribe";
+import { Footer } from "../Footer";
 
 export function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Subscribe />} />
-      <Route path="/event" element={<Event />} />
-      <Route path="/event/lesson/:slug" element={<Event />} />
-    </Routes>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Subscribe />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/event/lesson/:slug" element={<Event />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
