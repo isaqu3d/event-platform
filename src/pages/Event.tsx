9919782cf208071df9040a894ae7141517c58b95
Isaque <isaque.error404@gmail.com>
2022-09-01 11:52:36 -0300

    add footer compoent

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,10 +1,12 @@
-import { useParams } from 'react-router-dom'
-import { Header } from '../components/header/Header'
-import { Sidebar } from '../components/sidebar/Sidebar'
-import { Video } from '../components/video/Video'
+import { useParams } from "react-router-dom";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+
+import { Sidebar } from "../components/Sidebar";
+import { Video } from "../components/Video";
 
 export function Event() {
-  const { slug } = useParams<{ slug: string }>()
+  const { slug } = useParams<{ slug: string }>();
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -13,6 +15,7 @@ export function Event() {
         {slug ? <Video lessonSlug={slug} /> : <div className="flex-1" />}
         <Sidebar />
       </main>
+      <Footer />
     </div>
-  )
+  );
 }
